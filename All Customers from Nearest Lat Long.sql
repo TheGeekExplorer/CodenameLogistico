e86e8900774941606b9ce364dd8da572e73abfb7
The Geek Explorer <mark@markgreenall.uk>
2018-11-08 17:56:32 +0000

    Update All Customers from Nearest Lat Long.sql

--- All Customers from Nearest Lat Long.sql
+++ All Customers from Nearest Lat Long.sql
@@ -6,43 +6,43 @@ SELECT
     dc_coords.coords_longitude,
     
     ROUND((
-		6371 * acos(
-			cos(
-				radians(@latitude)
-			) * cos(
-				radians(dc_coords.coords_latitude)
-			) * cos(
-				radians(dc_coords.coords_longitude) - radians(@longitude)
-			) + sin(
-				radians(@latitude)
-			) * sin(
-				radians(dc_coords.coords_latitude)
-			)
-		)
-	), 2) AS distance_in_kilometers,
+        6371 * acos(
+            cos(
+                radians(@latitude)
+            ) * cos(
+                radians(dc_coords.coords_latitude)
+            ) * cos(
+                radians(dc_coords.coords_longitude) - radians(@longitude)
+            ) + sin(
+                radians(@latitude)
+            ) * sin(
+                radians(dc_coords.coords_latitude)
+            )
+        )
+    ), 2) AS distance_in_kilometers,
     
-	dc_customers.customerid,
-	dc_customers.business_name,
+    dc_customers.customerid,
+    dc_customers.business_name,
     dc_customers.address_house_no,
     dc_customers.address_street,
-	dc_customers.address_town,
+    dc_customers.address_town,
     dc_customers.address_county,
     dc_customers.address_postcode
 
 
 FROM
-	dc_coords
+    dc_coords
 
 USE INDEX
-	(idx_latitude, idx_longitude)
+    (idx_latitude, idx_longitude)
 
 INNER JOIN
-	dc_customers
+    dc_customers
     USE INDEX (idx_coordsid)
     ON dc_coords.coordsid = dc_customers.address_coordsid
 
 WHERE
-	dc_customers.status = 1
+    dc_customers.status = 1
 
 GROUP BY
     dc_coords.coords_latitude,
@@ -50,10 +50,10 @@ GROUP BY
     dc_customers.customerid
 
 HAVING
-	distance_in_kilometers <= 50
+    distance_in_kilometers <= 50
 
 ORDER BY
-	distance_in_kilometers ASC
+    distance_in_kilometers ASC
 
 LIMIT
-	0, 150
+    0, 150
