6f185412de2fa2680672efbbe6dde0921899965f
The Geek Explorer <mark@markgreenall.uk>
2018-11-09 15:00:00 +0000

    Update All Drops from Nearest Lat Long.sql

--- All Drops from Nearest Lat Long.sql
+++ All Drops from Nearest Lat Long.sql
@@ -2,23 +2,23 @@ SET @vehicleid          = 1;
 SET @distance           = 50;
 
 SELECT
+    # IDs for Vehicle, Drops, and Customers
+    dc_drops.dropid,
+    dc_customers.customerid       as customer_customerid,
+    dc_vehicles.vehicleid,
+    
+    
     # VEHICLE DETAILS
     dc_vehicles.location_latitude  as vehicle_latitude,
     dc_vehicles.location_longitude as vehicle_longitude,
     dc_vehicles.pallets_capacity   as vehicle_capacity,
     dc_vehicles.pallets_usage      as vehicle_usage,
-    SUM(dc_vehicles.pallets_capacity - dc_vehicles.pallets_usage) as vehicle_capacity_current,
+    (dc_vehicles.pallets_capacity - dc_vehicles.pallets_usage) as vehicle_capacity_current,
     
     
-    # CUSTOMER DETAILS
+    # DROP DETAILS
     dc_drops.pallets_count        as drop_pallets_count,
-    dc_customers.customerid       as customer_customerid,
-    dc_customers.business_name    as customer_business_name,
-    dc_customers.address_house_no as business_address_house_no,
-    dc_customers.address_street   as business_address_street,
-    dc_customers.address_town     as business_address_town,
-    dc_customers.address_county   as business_address_county,
-    dc_customers.address_postcode as business_address_postcode,
+    
     
     # DROP PICKUP/DROPOFF COORDS
     dc_drops.pickup_latitude,
@@ -88,6 +88,7 @@ SELECT
     dc_drops.pickup_county,
     dc_drops.pickup_postcode,
     
+    
     # DROP DROPOFF ADDRESS
     dc_drops.dropoff_house_no,
     dc_drops.dropoff_street,
@@ -95,22 +96,34 @@ SELECT
     dc_drops.dropoff_county,
     dc_drops.dropoff_postcode,
     
+    
     # PICKUP/DROPOFF TIME FROM/TO
     dc_drops.pickup_time_from,
     dc_drops.pickup_time_to,
     dc_drops.dropoff_time_from,
     dc_drops.dropoff_time_to,
-
+    
+    
     # PICKUP/DROPOFF DATE FROM/TO
     dc_drops.pickup_date_from,
     dc_drops.pickup_date_to,
     dc_drops.dropoff_date_from,
     dc_drops.dropoff_date_to,
     
+    
     # CONDITIONS
     dc_drops.fragile,
     dc_drops.perishable,
-    dc_drops.chilled
+    dc_drops.chilled,
+    
+    
+    # CUSTOMER DETAILS
+    dc_customers.business_name    as customer_business_name,
+    dc_customers.address_house_no as business_address_house_no,
+    dc_customers.address_street   as business_address_street,
+    dc_customers.address_town     as business_address_town,
+    dc_customers.address_county   as business_address_county,
+    dc_customers.address_postcode as business_address_postcode
     
     
 FROM
@@ -150,9 +163,11 @@ WHERE
         )
     )) <= @distance
     
+    
     # Calculate how many spare pallet slots in vehicle, and 
     # if this drop is less or equal to spare space
     AND (dc_vehicles.pallets_capacity - dc_vehicles.pallets_usage) <= dc_drops.pallets_count
+    
     
     # Check that the customers account has not been 
     # banned/deleted/disabled
@@ -160,20 +175,22 @@ WHERE
     
     
 GROUP BY
-    dc_drops.pallets_count,
+    # IDs for Vehicles, Drops, and Customers
+    dc_drops.dropid,
     dc_customers.customerid,
-    dc_customers.business_name,
-    dc_customers.address_house_no,
-    dc_customers.address_street,
-    dc_customers.address_town,
-    dc_customers.address_county,
-    dc_customers.address_postcode,
+    dc_vehicles.vehicleid,
+    
+    
+    # DROP DETAILS
+    dc_drops.pallets_count,
+    
     
     # PICKUP/DROPOFF COORDS
     dc_drops.pickup_latitude,
     dc_drops.pickup_longitude,
     dc_drops.dropoff_latitude,
     dc_drops.dropoff_longitude,
+    
     
     # PICKUP ADDRESS
     dc_drops.pickup_house_no,
@@ -182,6 +199,7 @@ GROUP BY
     dc_drops.pickup_county,
     dc_drops.pickup_postcode,
     
+    
     # DROPOFF ADDRESS
     dc_drops.dropoff_house_no,
     dc_drops.dropoff_street,
@@ -189,28 +207,44 @@ GROUP BY
     dc_drops.dropoff_county,
     dc_drops.dropoff_postcode,
     
+    
     # PICKUP/DROPOFF TIME FROM/TO
     dc_drops.pickup_time_from,
     dc_drops.pickup_time_to,
     dc_drops.dropoff_time_from,
     dc_drops.dropoff_time_to,
-
+    
+    
     # PICKUP/DROPOFF DATE FROM/TO
     dc_drops.pickup_date_from,
     dc_drops.pickup_date_to,
     dc_drops.dropoff_date_from,
     dc_drops.dropoff_date_to,
     
+    
     # CONDITIONS
     dc_drops.fragile,
     dc_drops.perishable,
-    dc_drops.chilled
-
+    dc_drops.chilled,
+    
+    
+    # CUSTOMER DETAILS
+    dc_customers.business_name,
+    dc_customers.address_house_no,
+    dc_customers.address_street,
+    dc_customers.address_town,
+    dc_customers.address_county,
+    dc_customers.address_postcode
+    
+    
 HAVING
     distance_calculation_driver_to_pickup <= @distance
-
+    
+    
 ORDER BY
     distance_calculation_driver_to_pickup ASC
-
+    
+    
 LIMIT
     0, 150
+    
